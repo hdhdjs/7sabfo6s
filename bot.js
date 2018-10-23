2c818d93ad2befb992f727499ea8c97df4472152
hdhdjs <43729658+hdhdjs@users.noreply.github.com>
2018-10-23 16:10:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,3 +32,38 @@ client.on('message', message => {
   });
 
 client.login(process.env.BOT_TOKEN);
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+});
+
+const devs = ['504265702891585546' , '' , '' , ''];
+const adminprefix = ".";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**!   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**!   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**!  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
+      message.channel.sendMessage(`**!   ${argresult}**`)
+  }
+  });
+
+client.login(process.env.BO_TOKEN);
